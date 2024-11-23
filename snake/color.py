bf50924e2193aaaad14fd58bbddd97713adbaaaa
alexgol123-bov <gregory321458@gmail.com>
2024-11-23 15:25:22 +0300

    Add score render

--- snake/color.py
+++ snake/color.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class Color(Enum):
+    WHITE = (200, 200, 200)
     GREY = (10, 10, 10)
     BLACK = (5, 5, 5)
     GREEN = (0, 230, 0)
--- snake/renderer.py
+++ snake/renderer.py
@@ -1,3 +1,5 @@
+from dataclasses import astuple
+
 import pygame
 
 from color import Color
@@ -12,10 +14,11 @@ class Renderer:
 
         self._fps = 16
         self._scale = Vector(10, 10)
-        self._frame_size = Vector(
+        self._board_frame_size = Vector(
             (self._model.board_size.x + 1) * self._scale.x,
             (self._model.board_size.y + 1) * self._scale.y,
         )
+        self._window_size = self._board_frame_size + Vector(200, 0)
 
         self.__init_window(title)
 
@@ -23,7 +26,9 @@ class Renderer:
         pygame.init()
         pygame.display.set_caption(title)
 
-        self._window = pygame.display.set_mode((self._frame_size.x, self._frame_size.y))
+        self._window = pygame.display.set_mode(astuple(self._window_size))
+        self._board_frame = pygame.Surface(astuple(self._board_frame_size))
+
         self._fps_controller = pygame.time.Clock()
 
     def __del__(self) -> None:
@@ -32,41 +37,54 @@ class Renderer:
     def render(self) -> None:
         self._clear_window()
 
-        self._draw_grid()
-        self._draw_apple()
-        self._draw_snake()
+        self._draw_board()
+        self._draw_score()
 
         pygame.display.update()
         self._update_fps()
 
     def _clear_window(self) -> None:
         self._window.fill(Color.BLACK.value)
+        self._board_frame.fill(Color.BLACK.value)
+
+    def _draw_board(self) -> None:
+        self._draw_grid()
+        self._draw_apple()
+        self._draw_snake()
+
+        self._window.blit(self._board_frame, (0, 0))
 
     def _draw_grid(self) -> None:
-        for x in range(1, self._frame_size.x // self._scale.x):
+        for x in range(self._model.board_size.x + 1):
             scaled_x = x * self._scale.x
             pygame.draw.line(
-                self._window,
+                self._board_frame,
                 Color.GREY.value,
                 (scaled_x, 0),
-                (scaled_x, self._frame_size.x),
+                (scaled_x, self._board_frame_size.x),
             )
 
-        for y in range(1, self._frame_size.y // self._scale.y):
+        for y in range(self._model.board_size.y + 1):
             scaled_y = y * self._scale.y
             pygame.draw.line(
-                self._window,
+                self._board_frame,
                 Color.GREY.value,
                 (0, scaled_y),
-                (self._frame_size.x, scaled_y),
+                (self._board_frame_size.x, scaled_y),
             )
+
+    def _draw_score(self) -> None:
+        margin = 16
+        font = pygame.font.SysFont("times", 16)
+        surface = font.render(f"Score: {self._model.score}", True, Color.WHITE.value)
+        self._window.blit(surface, (self._board_frame_size.x + margin, margin))
 
     def _draw_apple(self) -> None:
         self._draw_object(self._model.apple.location, Color.RED)
 
     def _draw_object(self, location: Vector, color: Color) -> None:
         pygame.draw.rect(
-            self._window,
+            self._board_frame,
             color.value,
             pygame.Rect(
                 location.x * self._scale.x,
