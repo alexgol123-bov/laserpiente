9e2e45e1234af04bdd56f5552e75cbfd326f0e54
alexgol123-bov <gregory321458@gmail.com>
2024-11-23 14:57:13 +0300

    Add grid

--- snake/renderer.py
+++ snake/renderer.py
@@ -12,7 +12,7 @@ class Renderer:
 
         self._fps = 16
         self._scale = Vector(10, 10)
-        self._frame_size = (
+        self._frame_size = Vector(
             (self._model.board_size.x + 1) * self._scale.x,
             (self._model.board_size.y + 1) * self._scale.y,
         )
@@ -23,7 +23,7 @@ class Renderer:
         pygame.init()
         pygame.display.set_caption(title)
 
-        self._window = pygame.display.set_mode(self._frame_size)
+        self._window = pygame.display.set_mode((self._frame_size.x, self._frame_size.y))
         self._fps_controller = pygame.time.Clock()
 
     def __del__(self) -> None:
@@ -32,8 +32,9 @@ class Renderer:
     def render(self) -> None:
         self._clear_window()
 
-        self._render_apple()
-        self._render_snake()
+        self._draw_grid()
+        self._draw_apple()
+        self._draw_snake()
 
         pygame.display.update()
         self._update_fps()
@@ -41,10 +42,29 @@ class Renderer:
     def _clear_window(self) -> None:
         self._window.fill(Color.BLACK.value)
 
-    def _render_apple(self) -> None:
-        self._render_object(self._model.apple.location, Color.RED)
+    def _draw_grid(self) -> None:
+        for x in range(1, self._frame_size.x // self._scale.x):
+            scaled_x = x * self._scale.x
+            pygame.draw.line(
+                self._window,
+                Color.GREY.value,
+                (scaled_x, 0),
+                (scaled_x, self._frame_size.x),
+            )
 
-    def _render_object(self, location: Vector, color: Color) -> None:
+        for y in range(1, self._frame_size.y // self._scale.y):
+            scaled_y = y * self._scale.y
+            pygame.draw.line(
+                self._window,
+                Color.GREY.value,
+                (0, scaled_y),
+                (self._frame_size.x, scaled_y),
+            )
+
+    def _draw_apple(self) -> None:
+        self._draw_object(self._model.apple.location, Color.RED)
+
+    def _draw_object(self, location: Vector, color: Color) -> None:
         pygame.draw.rect(
             self._window,
             color.value,
@@ -56,9 +76,9 @@ class Renderer:
             ),
         )
 
-    def _render_snake(self) -> None:
+    def _draw_snake(self) -> None:
         for location in self._model.snake.body:
-            self._render_object(location, Color.GREEN)
+            self._draw_object(location, Color.GREEN)
 
     def _update_fps(self) -> None:
         self._fps_controller.tick(self._fps)
