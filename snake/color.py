97a2962ce22b2c9f2e7a23165e550f9e45a894ac
alexgol123-bov <gregory321458@gmail.com>
2024-11-23 11:04:36 +0300

    Add a Game class

---
+++ snake/color.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Color(Enum):
+    BLACK = (0, 0, 0)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
---
+++ snake/main.py
@@ -0,0 +1,7 @@
+from game import Game
+
+
+if __name__ == "__main__":
+    title = "La Serpiente"
+    game = Game(title)
+    game.run()
