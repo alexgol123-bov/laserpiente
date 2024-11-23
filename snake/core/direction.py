5b8987694c5935bfc53599c0a3bb1f976bae644e
alexgol123-bov <gregory321458@gmail.com>
2024-11-23 11:02:53 +0300

    Add a direction class that represents the direction of the snake's movement

---
+++ snake/core/direction.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+from core.types.vector import Vector
+
+
+class Direction(Enum):
+    UP = Vector(0, -1)
+    DOWN = Vector(0, 1)
+    LEFT = Vector(-1, 0)
+    RIGHT = Vector(1, 0)
